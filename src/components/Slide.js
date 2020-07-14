5aac20a168425ca5cfb5b00dea93f12efdd6339d
brennop <brennopcordeiro@gmail.com>
2020-07-14 19:59:36 -0300

    📦 Adiciona atributos para o markdown

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import Markdown from "markdown-it";
 import emoji from "markdown-it-emoji";
+import attrs from "markdown-it-attrs";
 
 const md = new Markdown();
 md.use(emoji);
+md.use(attrs);
 
 const Slide = ({ value }) => {
   const section = useRef();
