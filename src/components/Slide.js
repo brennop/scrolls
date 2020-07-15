4a8a3b9c5fa9d1a3a8ddee2745890f1d7dc5ad3d
brennop <brennopcordeiro@gmail.com>
2020-07-15 14:04:15 -0300

    😁 Adiciona o twemoji 🎉

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -2,10 +2,15 @@ import React, { useEffect, useRef } from "react";
 import Markdown from "markdown-it";
 import emoji from "markdown-it-emoji";
 import attrs from "markdown-it-attrs";
+import twemoji from "twemoji";
 
 const md = new Markdown();
 md.use(emoji);
 md.use(attrs);
+
+md.renderer.rules.emoji = function (token, idx) {
+  return twemoji.parse(token[idx].content);
+};
 
 const Slide = ({ value }) => {
   const section = useRef();
