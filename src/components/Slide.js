0f7ce4d413b4ca7ba50b9c64a2d872bea879c87e
brennop <brennopcordeiro@gmail.com>
2020-10-21 10:34:35 -0300

    🎨 melhora o highlighting

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,42 +1,6 @@
 import React, { useEffect, useRef } from "react";
-import twemoji from "twemoji";
 import styled from "@emotion/styled";
-
-import Markdown from "markdown-it";
-import emoji from "markdown-it-emoji";
-import attrs from "markdown-it-attrs";
-import mila from "markdown-it-link-attributes";
-
-import Prism from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
-import "prismjs/components/prism-bash";
-
-const md = new Markdown({
-  linkify: true,
-  highlight: (code, lang) => {
-    if (lang && Prism.languages[lang]) {
-      try {
-        return Prism.highlight(code, Prism.languages[lang]);
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  },
-});
-
-md.use(emoji);
-md.use(attrs);
-md.use(mila, {
-  attrs: {
-    target: "_blank",
-    rel: "noopener noreferer",
-  },
-});
-
-md.renderer.rules.emoji = function (token, idx) {
-  return twemoji.parse(token[idx].content);
-};
+import { md } from "../utils/markdown";
 
 const Wrapper = styled.section`
   display: flex;
---
+++ src/utils/markdown.js
@@ -0,0 +1,48 @@
+import twemoji from "twemoji";
+
+import Markdown from "markdown-it";
+import emoji from "markdown-it-emoji";
+import attrs from "markdown-it-attrs";
+import mila from "markdown-it-link-attributes";
+
+import {
+  highlight,
+  languages,
+  highlightAll,
+} from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+
+export const md = new Markdown({
+  linkify: true,
+  highlight: (code, lang) => {
+    if (lang) {
+      if (languages[lang]) {
+        try {
+          return highlight(code, languages[lang]);
+        } catch (e) {
+          console.log(e);
+        }
+      } else {
+        import("prismjs/components/prism-" + lang)
+          .then(() => highlightAll())
+          .catch((error) => console.log(error));
+      }
+    }
+  },
+});
+
+// plugins
+md.use(emoji);
+md.use(attrs);
+md.use(mila, {
+  attrs: {
+    target: "_blank",
+    rel: "noopener noreferer",
+  },
+});
+
+// twitter emojis
+md.renderer.rules.emoji = function (token, idx) {
+  return twemoji.parse(token[idx].content);
+};
