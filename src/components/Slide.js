4425920b41bfe864199578d6d42401c546dcc719
brennop <brennopcordeiro@gmail.com>
2020-10-21 09:28:36 -0300

    🔗 abre links em nova aba

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useRef } from "react";
-import Markdown from "markdown-it";
-import emoji from "markdown-it-emoji";
-import attrs from "markdown-it-attrs";
 import twemoji from "twemoji";
 import styled from "@emotion/styled";
 
-const md = new Markdown();
+import Markdown from "markdown-it";
+import emoji from "markdown-it-emoji";
+import attrs from "markdown-it-attrs";
+import mila from "markdown-it-link-attributes";
+
+const md = new Markdown({ linkify: true });
 md.use(emoji);
 md.use(attrs);
+md.use(mila, {
+  attrs: {
+    target: "_blank",
+    rel: "noopener noreferer",
+  },
+});
 
 md.renderer.rules.emoji = function (token, idx) {
   return twemoji.parse(token[idx].content);
