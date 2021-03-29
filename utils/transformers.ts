8da209491846a6557de4dc4384ff0a87cf89e24b
brennop <brennopcordeiro@gmail.com>
2021-03-29 10:33:30 -0300

    style: Make codesandbox occupy full space

--- utils/transformers.ts
+++ utils/transformers.ts
@@ -41,7 +41,7 @@ const CodeSandboxTransformer: Transformer = {
   getHTML(url) {
     const iframeUrl = url.replace('/s/', '/embed/');
 
-    return `<iframe src="${iframeUrl}" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"></iframe>`;
+    return `<iframe src="${iframeUrl}" style="width:100%; height:100%; border:0; border-radius: 4px; overflow:hidden;" allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"></iframe>`;
   },
 };
 
